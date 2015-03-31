2985cee71a1408c0db47970fff9b620ffba1987a
Ryan Hileman <lunixbochs@gmail.com>
2015-03-31 07:24:16 -0700

    cleanup mul_vec4

--- matrix.h
+++ matrix.h
@@ -102,8 +102,7 @@ void mat4_mul_vec3(mat4 *m, float out[3], const float in[3]) {
 }
 
 void mat4_mul_vec4(mat4 *m, float out[4], const float in[4]) {
-    simd4f coord = simd4f_create(in[0], in[1], in[2], in[3]);
-    simd4f tmp;
-    simd4x4f_matrix_vector_mul(m, &coord, &tmp);
-    simd4f_ustore4(coord, out);
+    simd4f tmp, vert = simd4f_create(in[0], in[1], in[2], in[3]);
+    simd4x4f_matrix_vector_mul(m, &vert, &tmp);
+    simd4f_ustore4(vert, out);
 }
