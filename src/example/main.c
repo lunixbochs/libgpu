ba0a0a74cf078dfee609b3717775462ecb3fc010
Ryan Hileman <lunixbochs@gmail.com>
2015-05-10 10:30:00 -0700

    use a better sdl2 draw method

--- src/example/main.c
+++ src/example/main.c
@@ -67,10 +67,18 @@ void draw_frame(uint8_t *frame_out, int counter) {
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("libgpu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
-    SDL_Surface *screen = SDL_GetWindowSurface(window);
-
-    SDL_Surface *tmp = SDL_CreateRGBSurface(0, 640, 480, 32, 0xFF, 0xFF00, 0xFF0000, 0xFF000000);
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+    SDL_Surface *screen = SDL_CreateRGBSurface(0, 640, 480, 32,
+                                               0x00FF0000,
+                                               0x0000FF00,
+                                               0x000000FF,
+                                               0xFF000000);
+    SDL_Texture *sdlTexture = SDL_CreateTexture(renderer,
+                                                SDL_PIXELFORMAT_ARGB8888,
+                                                SDL_TEXTUREACCESS_STREAMING,
+                                                640, 480);
     int i = 0;
     bool done = false;
     bool click = false;
@@ -94,11 +102,13 @@ int main() {
                     break;
             }
         }
-        SDL_LockSurface(tmp);
-        draw_frame(tmp->pixels, i);
-        SDL_UnlockSurface(tmp);
-        SDL_BlitSurface(tmp, NULL, screen, NULL);
-        SDL_UpdateWindowSurface(window);
+        SDL_LockSurface(screen);
+        draw_frame(screen->pixels, i);
+        SDL_UnlockSurface(screen);
+        SDL_UpdateTexture(sdlTexture, NULL, screen->pixels, screen->pitch);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, sdlTexture, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
     SDL_DestroyWindow(window);
     SDL_Quit();
